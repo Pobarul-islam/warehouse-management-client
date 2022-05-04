f11a29523519c2b63fb76aa0a814dbb11baeca8a
unknown <Pobarul-islam>
2022-05-04 09:04:01 +0600

    progress info added

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       <Header></Header>
       <Routes>
        
+        <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/checkout' element={<Checkout></Checkout>}></Route>
         <Route path='/services' element={<Services></Services>}></Route>
--- src/Components/Progress/Progress.js
+++ src/Components/Progress/Progress.js
@@ -7,22 +7,22 @@ const Progress = () => {
           
             <div className=' container mt-5'>
                 <div className='row'>
-                    <div className='col'><h4>Our Client</h4>
-                        <h6>Total watches</h6>
-                        <p>1040+</p>
+                    <div className='col-3 card text-center '><h4>Total Watches</h4>
+                        <h6>504738+</h6>
                     </div>
-                    <div className='col'><h4>Our Client</h4>
-                        <h6>Total watches</h6>
-                        <p>1040+</p>
+                    <div className='col-3 card text-center'><h4></h4>
+                        <h4>Ourt Client</h4>
+                        <h6>89680+</h6>
                     </div>
-                    <div className='col'><h4>Our Client</h4>
-                        <h6>Total watches</h6>
-                        <p>1040+</p>
+                    <div className='col-3 card text-center'><h4>Service Point</h4>
+                        <h6>7860+</h6>
+                        
                     </div>
-                    <div className='col'><h4>Our Client</h4>
-                        <h6>Total watches</h6>
-                        <p>1040+</p>
+                    <div className='col-3 card text-center'><h4>Warranty Service</h4>
+                        <h6>78987+</h6>
+                        
                     </div>
+                   
                 </div>
             </div>
         </div>
