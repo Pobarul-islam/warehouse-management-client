6aab33887b211efdb94b54f2b079f79919f9bfe0
unknown <Pobarul-islam>
2022-05-04 18:23:12 +0600

    watches page protected use login form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,30 +1,79 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import './Login.css';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    if (user) {
+        navigate('/services');
+    }
+    if (user == password) {
+      error('not match')
+  }
+
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+
     return (
-        <div className='login-page mx-auto'>
-            <h2 className='text-center'>Please Login !!!</h2>
-            <Form className='card p-5'>
+
+        
+        <div className='singup-form mx-auto'>
+            <h2 className='text-center'>Please Login </h2>
+
+          
+
+            <Form onSubmit={handleUserSignIn} className='card p-5'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
 
-                    <Form.Control type="email" placeholder="Enter email" required/>
+                    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
 
                 </Form.Group>
-
+                {
+                    loading && <p><div class="spinner-border text-warning" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div></p>
+                }
                 <Form.Group className="mb-3" controlId="formBasicPassword">
 
-                    <Form.Control type="password" placeholder="Password" required/>
+                    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
                 </Form.Group>
+
+              
+                    
 
                 <Button className='form-submit' variant="primary" type="submit">
                     Login
                 </Button>
+                
+
+            
             </Form>
-            New to Stock Watches ? <Link to="/singup">Please Create an account</Link>
+            New to Stock Watches ? <Link className='text-decoration-none' to="/singup">Please Create an account !!</Link>
         </div>
     );
 };
