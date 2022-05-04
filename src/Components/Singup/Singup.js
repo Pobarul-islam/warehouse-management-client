a2b89e83877d2c57bd5d460028e4ed1d76fbfbdf
unknown <Pobarul-islam>
2022-05-04 16:29:52 +0600

    set password condition

--- src/Components/Singup/Singup.js
+++ src/Components/Singup/Singup.js
@@ -1,33 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Singup.css'
 
 const Singup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleConfirmPassword = event => {
+        setConfirmPassword(event.target.value);
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+    }
+
+    if (password !== confirmPassword) {
+        setError('Your two password did not match')
+        return;
+    }
     return (
         <div className='singup-form mx-auto'>
             <h2 className='text-center'>Please Singup !!!</h2>
 
-            <Form className='card p-5'>
+            <Form onSubmit={handleCreateUser} className='card p-5'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     
-                    <Form.Control type="email" placeholder="Enter email" required/>
+                    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required/>
 
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
             
-                    <Form.Control type="password" placeholder="Password" required/>
+                    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   
-                    <Form.Control type="password" placeholder="Confirm Password" required />
+                    <Form.Control onBlur={handleConfirmPassword} type="password" placeholder="Confirm Password" required />
                 </Form.Group>
 
                 <Button className='form-submit' variant="primary" type="submit">
                     Singup
                 </Button>
+                <p style={{color:"red"}}>{ error}</p>
             </Form>
             Already have an account ? <Link to="/login">Please Login</Link>
         </div>
