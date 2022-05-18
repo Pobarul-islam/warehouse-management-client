990de570940515481832db2d23e3427ae25ebe7d
unknown <Pobarul-islam>
2022-05-18 18:52:59 +0600

    add product added use reacr hook from

--- src/Components/AddItems/AddItems.js
+++ src/Components/AddItems/AddItems.js
@@ -1,16 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddItems = () => {
-    const [product, setProduct] = useState({})
-    useEffect(() => {
-        fetch('http://localhost:5000/service')
-            .then(res => res.json())
-            .then(data => setProduct(data))
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/service`
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
 
-    }, [])
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result)
+            })
+
+
+    };
+
     return (
-        <div>
-            <h1>name: {product.name}</h1>
+        <div className='w-50 mx-auto'>
+            <h2>Add Product</h2>
+            <form className='d-flex flex-column mt-5' onSubmit={handleSubmit(onSubmit)}>
+                <input className='mb-2' placeholder='Photo URL' type="text" {...register("img")} />
+                <input className='mb-2' placeholder='Name' {...register("name")} />
+                <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+                <input className='mb-2' placeholder='Suppllyer Name' {...register("name")} />
+                <textarea className='mb-2' placeholder='Description' {...register("description", { required: true, maxLength: 20 })} />
+
+
+
+                <input type="Submit" value="Add Product" />
+            </form>
+
+
         </div>
     );
 };
